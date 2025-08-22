e9bc6788b36e93001ad740b341fdbd7b526e4617
jaymwin <jmwiniarski@gmail.com>
2025-08-22 11:23:57 -0500

    corrected db credentials function

--- R/cb_set_db_credentials.R
+++ R/cb_set_db_credentials.R
@@ -14,9 +14,9 @@ cb_set_db_credentials <- function(credentials_path) {
 
   credentials_df <- readr::read_csv(credentials_path)
 
-  keyring::key_set(credentials_df$host)
-  keyring::key_set(credentials_df$user)
-  keyring::key_set(credentials_df$password)
-  keyring::key_set(credentials_df$db)
+  keyring::key_set_with_value(service = 'spotted_owl_host', password = credentials_df$host)
+  keyring::key_set_with_value(service = 'spotted_owl_user', password = credentials_df$user)
+  keyring::key_set_with_value(service = 'spotted_owl_pwd', password = credentials_df$password)
+  keyring::key_set_with_value(service = 'spottedowl_db', password = credentials_df$db)
 
 }
