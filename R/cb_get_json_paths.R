03ccb4b3e8eab191a24b7b51f841aa5895cffe0c
jaymwin <jmwiniarski@gmail.com>
2025-09-08 15:48:05 -0500

    change json path function

--- R/cb_get_json_paths.R
+++ R/cb_get_json_paths.R
@@ -1,40 +1,33 @@
 
 #' Get path info for JSONs on research drive
 #'
-#' @param path Path to JSON files
-#' @param year Year
+#' @param root_path Path to JSON files
 #'
-#' @return Tibble of JSON file paths with size (MB), file name, and deployment name
+#' @return Tibble of JSON file paths with size (MB) and file name
 #' @export
 #'
 #' @examples
 
-cb_get_json_paths <- function(path, year) {
+cb_get_json_paths <- function(root_path) {
 
-  df <-
-    fs::dir_ls(
-      path = path,
-      recurse = 2,
-      regex = as.character(year)
-    ) |>
-    tibble::as_tibble() |>
-    dplyr::rowwise() |>
-    dplyr::mutate(depth = length(strsplit(value, "/")[[1]])) |>
-    dplyr::ungroup() |>
-    dplyr::filter(depth == max(depth)) |>
-    dplyr::select(-depth)
+  # Temporary output file for the listing
+  tmp_csv <- tempfile(fileext = ".csv")
 
-  subfolder_contents <-
-    df |>
-    dplyr::group_split(value) |>
-    purrr::map(get_subdirectory_contents) |>
-    purrr::list_c()
+  # PowerShell command
+  ps_cmd <-
+    paste0(
+      "powershell -Command \"",
+      "Get-ChildItem -Path '", root_path, "' -Recurse -Include *.json.gz -File | ",
+      "Select-Object @{Name='path';Expression={$_.FullName}}, ",
+      "@{Name='bytes';Expression={$_.Length}} | ",
+      "Export-Csv -Path '", tmp_csv, "' -NoTypeInformation\""
+    )
 
-  raw_file_info_df <-
-    subfolder_contents |>
-    furrr::future_map(get_file_info) |>
-    dplyr::bind_rows()
+  system(ps_cmd)
 
-  return(raw_file_info_df)
+  # Read the csv straight into R
+  json_file_info_df <- readr::read_csv(tmp_csv, show_col_types = FALSE)
+
+  return(json_file_info_df)
 
 }
