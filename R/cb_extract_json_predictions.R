44eceaf79e24f6ffaea35884552ae59d4ce09d87
jaymwin <jmwiniarski@gmail.com>
2025-09-17 10:05:08 -0500

    updated birdnet extraction function to better log json parsing errors/successes and allow for forest owl extraction at 06:00:00 and 07:00:00

--- R/cb_extract_json_predictions.R
+++ R/cb_extract_json_predictions.R
@@ -1,140 +1,155 @@
 
 #' Extract predictions from JSON files using species-specific threshold values
 #'
-#' @param path
-#' @param species_thresholds
-#' @param date_time
+#' @param json
+#' @param threshold_df
+#' @param predictions_output
+#' @param log_output
 #'
 #' @return
 #' @export
 #'
 #' @examples
 
-cb_extract_json_predictions <- function(path, species_thresholds = species_threshold_df, date_time = date_time) {
+cb_extract_json_predictions <- function(json, threshold_df, predictions_output, log_output) {
 
   # get name of file first for saving things later
-  file_name <- stringr::str_extract(path, 'G(P|R|C|M|0)[0-9]{2}_V[1-5]{1}_C[0-9]{4}_U[1-5]{1}_[0-9]{8}_[0-9]{6}')
-  # and start time of the file
-  file_date_time <- lubridate::ymd_hms(stringr::str_extract(file_name, '[0-9]{8}_[0-9]{6}'))
-  rounded_file_start_time <- hms::as_hms(lubridate::round_date(file_date_time, 'hour'))
+  json_name <-
+    stringr::str_remove(basename(json), '.json.gz')
 
-  # read json (there shouldn't be any errors at this point)
+  # get file date time and start hour recording to determine which species to extract predictions for
+  json_date_time <-
+    lubridate::ymd_hms(stringr::str_extract(json_name, '[0-9]{8}_[0-9]{6}'), tz = 'America/Los_Angeles')
+
+  # hour
+  rounded_json_start_time <-
+    hms::as_hms(lubridate::round_date(json_date_time, 'hour'))
+
+  # try reading and parsing JSON
   json_df <-
-    jsonlite::fromJSON(path) |>
-    # turn into data frame where every row is a list
-    tibble::enframe() |>
-    # get detections part
-    dplyr::filter(name == 'detections') |>
-    # do some unnesting/cleaning
-    tidyr::unnest_longer(value) |>
-    dplyr::select(-name) |>
-    dplyr::rename(relative_time = value_id) |>
-    tidyr::unnest(cols = c(value)) |>
-    tidyr::unnest(cols = c(value))
+    tryCatch({
+      jsonlite::fromJSON(json) |>
+        tibble::enframe() |>
+        dplyr::filter(name == 'detections') |>
+        tidyr::unnest_longer(value) |>
+        dplyr::select(-name) |>
+        dplyr::rename(relative_time = value_id) |>
+        tidyr::unnest(cols = c(value)) |>
+        tidyr::unnest(cols = c(value))
+    }, error = function(e) {
+      message("Failed to parse: ", json, " — ", e$message)
 
-  # some can parse but have no detections; save # of predictions per file here
-  tibble::tibble(
-    path = basename(file_name),
-    n_predictions = dim(json_df)[1]
-  ) |>
+      # Log to a failures file
+      tibble::tibble(json = json, error = e$message) |>
+        write.table(
+          stringr::str_glue('{log_output}/json_failures.txt'),
+          col.names = FALSE,
+          row.names = FALSE,
+          append = TRUE,
+          sep = "\t",
+          quote = TRUE
+        )
+
+      return(NULL)
+
+    })
+
+  if (is.null(json_df)) {
+
+    return(NULL)
+
+  }
+
+  # get predictions if everything parsed above
+  # and do so depending on the hour of the recording
+  if (rounded_json_start_time %in% all_bird_hours) {
+
+    # all birds (diurnal, forest owl, csow, bdow)
+    json_df <-
+      json_df |>
+      dplyr::group_by(relative_time) |>
+      dplyr::mutate(
+        species_code = dplyr::row_number() - 1,
+        relative_time = as.numeric(relative_time)
+      ) |>
+      dplyr::ungroup() |>
+      dplyr::inner_join(
+        threshold_df,
+        by = dplyr::join_by('species_code')
+      ) |>
+      dplyr::filter(value >= logit_threshold)
+
+  } else if (rounded_json_start_time %in% csow_bdow_forest_owl_hours) {
+
+    # csow/bdow, forest owls
+    json_df <-
+      json_df |>
+      dplyr::group_by(relative_time) |>
+      dplyr::mutate(
+        species_code = dplyr::row_number() - 1,
+        relative_time = as.numeric(relative_time)
+      ) |>
+      dplyr::ungroup() |>
+      dplyr::inner_join(
+        threshold_df |> dplyr::filter(species_type %in% c('csow_bdow', 'forest_owl')),
+        by = dplyr::join_by('species_code')
+      ) |>
+      dplyr::filter(value >= logit_threshold)
+
+  } else if (rounded_json_start_time %in% diurnal_bird_forest_owl_hours) {
+
+    # diurnal birds and forest owls
+    json_df <-
+      json_df |>
+      dplyr::group_by(relative_time) |>
+      dplyr::mutate(
+        species_code = dplyr::row_number() - 1,
+        relative_time = as.numeric(relative_time)
+      ) |>
+      dplyr::ungroup() |>
+      dplyr::inner_join(
+        threshold_df |> dplyr::filter(species_type %in% c('diurnal', 'forest_owl')),
+        by = dplyr::join_by('species_code')
+      ) |>
+      dplyr::filter(value >= logit_threshold)
+
+  } else if (rounded_json_start_time %in% diurnal_bird_hours) {
+
+    # diurnal birds only
+    json_df <-
+      json_df |>
+      dplyr::group_by(relative_time) |>
+      dplyr::mutate(
+        species_code = dplyr::row_number() - 1,
+        relative_time = as.numeric(relative_time)
+      ) |>
+      dplyr::ungroup() |>
+      dplyr::inner_join(
+        threshold_df |> dplyr::filter(species_type == 'diurnal'),
+        by = dplyr::join_by('species_code')
+      ) |>
+      dplyr::filter(value >= logit_threshold)
+
+  }
+
+  # keep any predictions above species-specific thresholds
+  if (nrow(json_df) > 0) {
+
+    # save to file based on json name
+    json_df |>
+      dplyr::mutate(json = json_name) |>
+      dplyr::select(json, relative_time, species_code, common_name, birdnet_logit = value) |>
+      arrow::write_parquet(stringr::str_glue('{predictions_output}/{json_name}_filtered.parquet'))
+
+  }
+
+  # log successfully processed JSON
+  tibble::tibble(json = json, n = nrow(json_df)) |>
     write.table(
-      stringr::str_glue(here::here('code_outputs/post_birdnet_{date_time}/prediction_count_jsons.txt')),
+      stringr::str_glue('{log_output}/json_successes.txt'),
       col.names = FALSE,
       row.names = FALSE,
       append = TRUE
     )
-
-  # get predictions for times just corresponding to CSOW/BDOW/forest owls
-  if (rounded_file_start_time %in% csow_bdow_forest_owl_hours) {
-
-    # owl outputs only
-    json_df <-
-      json_df |>
-      dplyr::group_by(relative_time) |>
-      # this allows for different duration files
-      dplyr::mutate(
-        species_code = dplyr::row_number() - 1,
-        relative_time = as.numeric(relative_time)
-      ) |>
-      dplyr::ungroup() |>
-      dplyr::inner_join(
-        species_threshold_df |> dplyr::filter(species_type %in% c('forest_owl', 'csow_bdow')),
-        by = dplyr::join_by('species_code')
-      ) |>
-      dplyr::filter(value >= logit_threshold)
-
-    # and save if there are any predictions above the thresholds
-    if (dim(json_df)[1] > 0) {
-
-    json_df <-
-      json_df |>
-      dplyr::group_by(relative_time, scientific_name) |>
-      dplyr::filter(species_code == max(species_code)) |>
-      dplyr::ungroup()
-
-    }
-
-    # get predictions for times corresponding to all birds
-  } else if (rounded_file_start_time %in% all_bird_hours) {
-
-    # all outputs
-    json_df <-
-      json_df |>
-      dplyr::group_by(relative_time) |>
-      # this allows for different duration files
-      dplyr::mutate(
-        species_code = dplyr::row_number() - 1,
-        relative_time = as.numeric(relative_time)
-      ) |>
-      dplyr::ungroup() |>
-      dplyr::inner_join(
-        species_threshold_df,
-        by = dplyr::join_by('species_code')
-      ) |>
-      dplyr::filter(value >= logit_threshold)
-
-    # and save if there are any predictions above thresholds
-    if (dim(json_df)[1] > 0) {
-
-      json_df <-
-        json_df |>
-        dplyr::group_by(relative_time, scientific_name) |>
-        dplyr::filter(species_code == max(species_code)) |>
-        dplyr::ungroup()
-
-    }
-
-    # get predictions for times just corresponding to diurnal birds
-  } else if (rounded_file_start_time %in% diurnal_bird_hours) {
-
-    # diurnal outputs only
-    json_df <-
-      json_df |>
-      dplyr::group_by(relative_time) |>
-      # this allows for different duration files
-      dplyr::mutate(
-        species_code = dplyr::row_number() - 1,
-        relative_time = as.numeric(relative_time)
-      ) |>
-      dplyr::ungroup() |>
-      dplyr::inner_join(
-        species_threshold_df |> dplyr::filter(species_type == 'diurnal'),
-        by = dplyr::join_by('species_code')
-      ) |>
-      dplyr::filter(value >= logit_threshold)
-
-  }
-
-  # and save if there are any predictions above thresholds
-  if (dim(json_df)[1] > 0) {
-
-    json_df |>
-      dplyr::select(-logit_threshold) |>
-      dplyr::mutate(json = file_name) |>
-      dplyr::select(json, relative_time, species_code, birdnet_logit = value) |>
-      # and save
-      readr::write_csv(stringr::str_glue(here::here('code_outputs/post_birdnet_{date_time}/species_predictions/{file_name}_filtered.csv')))
-
-  }
 
 }
