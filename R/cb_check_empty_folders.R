57918ccc04ea2da77bdd1fa75f507a5ee6ced6c6
jaymwin <jmwiniarski@gmail.com>
2025-09-24 11:01:22 -0500

    streamlined checking for empty folders

--- R/cb_check_empty_folders.R
+++ R/cb_check_empty_folders.R
@@ -1,72 +1,42 @@
 
 #' Check for folders missing acoustic data on research drive
 #'
-#' @param path
-#' @param year
+#' @param root
 #'
 #' @return
 #' @export
 #'
 #' @examples
 
-cb_check_empty_folders <- function(path, year) {
+cb_check_empty_folders <- function(root) {
 
-  # get monitoring/project subfolders for a particular year
-  df <-
-    fs::dir_ls(
-      path = path,
-      regex = as.character(year)
-    ) |>
-    tibble::as_tibble() |>
-    dplyr::rowwise() |>
-    dplyr::mutate(depth = length(strsplit(value, "/")[[1]])) |>
-    dplyr::ungroup() |>
-    dplyr::filter(depth == max(depth)) |>
-    dplyr::select(-depth)
+  # Temporary output file for the listing
+  tmp_csv <- tempfile(fileext = ".csv")
 
-  # get group_visit folders within a region folder
-  folders_0 <-
-    df |>
-    dplyr::pull(value) |>
-    furrr::future_map_dfr(get_files_next_depth) |>
-    dplyr::rename(region = upper_level) |>
-    dplyr::select(region, group_visit = value)
+  # PowerShell command
+  ps_cmd <-
+    paste0(
+      "powershell -Command \"",
+      "Get-ChildItem -Path '", root, "' -Recurse -Directory | ",
+      "Where-Object { -Not (Get-ChildItem -Path $_.FullName -Recurse -Force -ErrorAction SilentlyContinue) } | ",
+      "Select-Object @{Name='path';Expression={$_.FullName}} | ",
+      "Export-Csv -Path '", tmp_csv, "' -NoTypeInformation\""
+    )
 
-  # get group_visit_cell_unit folders within a group_visit folder
-  folders_1 <-
-    folders_0 |>
-    tidyr::drop_na() |>
-    dplyr::pull(group_visit) |>
-    furrr::future_map_dfr(get_files_next_depth) |>
-    dplyr::rename(group_visit = upper_level) |>
-    dplyr::select(group_visit, group_visit_cell_unit = value)
+  system(ps_cmd)
 
-  # get group_visit_cell_unit_date folders within a group_visit_cell_unit folder
-  folders_2 <-
-    folders_1 |>
-    tidyr::drop_na() |>
-    dplyr::pull(group_visit_cell_unit) |>
-    furrr::future_map_dfr(get_files_next_depth) |>
-    dplyr::mutate(group_visit_cell_unit = upper_level) |>
-    dplyr::select(group_visit_cell_unit, group_visit_cell_unit_date = value)
+  # Read the csv straight into R
+  empty_dirs_df <- readr::read_csv(tmp_csv, show_col_types = FALSE)
 
-  # get flacs within a group_visit_cell_unit_date folder
-  folders_3 <-
-    folders_2 |>
-    tidyr::drop_na() |>
-    dplyr::pull(group_visit_cell_unit_date) |>
-    furrr::future_map_dfr(get_files_next_depth) |>
-    dplyr::mutate(group_visit_cell_unit_date = upper_level) |>
-    dplyr::select(group_visit_cell_unit_date, group_visit_cell_unit_date_hour = value)
+  # fix up paths if there are any empty directories
+  if (dim(empty_dirs_df)[1] > 0) {
 
-  # combine all empty folders from above
-  empty_df <-
-    folders_0 |> dplyr::filter(is.na(group_visit)) |> dplyr::select(folder = region) |>
-    dplyr::bind_rows(folders_1 |> dplyr::filter(is.na(group_visit_cell_unit)) |> dplyr::select(folder = group_visit)) |>
-    dplyr::bind_rows(folders_2 |> dplyr::filter(is.na(group_visit_cell_unit_date)) |> dplyr::select(folder = group_visit_cell_unit)) |>
-    dplyr::bind_rows(folders_3 |> dplyr::filter(is.na(group_visit_cell_unit_date_hour)) |> dplyr::select(folder = group_visit_cell_unit_date)) |>
-    dplyr::rename(empty_folder = folder)
+    empty_dirs_df <-
+      empty_dirs_df |>
+      mutate(path = stringr::str_replace_all(path, '\\\\', '/'))
 
-  return(empty_df)
+  }
+
+  return(empty_dirs_df)
 
 }
