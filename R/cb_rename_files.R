e78760cc8ce6b7b9afa85df39695bbbff8f6208b
jaymwin <jmwiniarski@gmail.com>
2025-10-29 07:39:20 -0500

    function changes didn't carry over yesterday for some reason, fixing now

--- R/cb_rename_files.R
+++ R/cb_rename_files.R
@@ -1,7 +1,8 @@
 
-#' Rename FLAC files with incorrect deployment information
+#' Rename FLACs, JSONs, and BirdNET selection tables (and associated directories) with incorrect deployment information
 #'
-#' @param dir Directory where FLACs are kept (for example, folder containing all FLACs for an ARU deployment)
+#' @param dir Directory where files are kept (for example, folder containing all FLACs for an ARU deployment)
+#' @param file_type .flac, .json.gz, or .BirdNET
 #' @param pattern String needing replacing
 #' @param replacement String to replace it with
 #'
@@ -10,21 +11,26 @@
 #'
 #' @examples
 #' \dontrun{
-#' # correct G001_C0001_V1_U2 to G001_C0001_V1_U1
-#' cb_rename_flacs(
+#' # correct G001_C0001_V1_U2 to G001_C0001_V1_U1 for a set of FLACs
+#' cb_rename_files(
 #'   dir = 'path/to/deployments/G001_C0001_V1_U2',
+#'   file_type = '.flac',
 #'   pattern = 'U2',
 #'   replacement = 'U1'
 #'   )
 #' }
 
-cb_rename_flacs <- function(dir, pattern, replacement) {
+cb_rename_files <- function(dir, file_type, pattern, replacement) {
+
+  if (!file_type %in% c(".flac", ".json.gz", ".BirdNET")) {
+    stop("Unsupported file type. Please provide a .flac, .json.gz, or .BirdNET file.")
+  }
 
   # get list of directories to change
   dirs <- fs::dir_ls(dir, recurse = TRUE, type = 'directory')
 
   # get list of flacs to change
-  flacs <- fs::dir_ls(dir, recurse = TRUE, glob = '*.flac')
+  files_to_rename <- fs::dir_ls(dir, recurse = TRUE, glob = stringr::str_glue('*{file_type}'))
 
   # create new directories first
   dirs |>
@@ -33,8 +39,8 @@ cb_rename_flacs <- function(dir, pattern, replacement) {
 
   # then move files (i.e., rename)
   fs::file_move(
-    path = flacs,
-    new_path = stringr::str_replace_all(flacs, pattern, replacement)
+    path = files_to_rename,
+    new_path = stringr::str_replace_all(files_to_rename, pattern, replacement)
   )
 
 }
