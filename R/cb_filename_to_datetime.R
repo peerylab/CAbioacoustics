34a52aae7796e277baa35e50bcb4e8ae72f47933
jaymwin <jmwiniarski@gmail.com>
2026-02-11 13:50:21 -0600

    update datetime function

--- R/cb_filename_to_datetime.R
+++ R/cb_filename_to_datetime.R
@@ -3,6 +3,7 @@
 #'
 #' @param x Path or filename
 #' @param type Datetime or date
+#' @param time_zone Timezone of files
 #'
 #' @return Datetime or date in PDT
 #' @export
@@ -15,27 +16,27 @@
 #'   )
 #'
 #' # get datetime
-#' cb_filename_to_datetime(df$filename, 'datetime')
+#' cb_filename_to_datetime(df$filename, 'datetime', timezone = 'America/Los_angeles')
 #'
 #' # or date
-#' cb_filename_to_datetime(df$filename, 'date')
+#' cb_filename_to_datetime(df$filename, 'date', timezone = 'America/Los_angeles')
 #'
 #' # or using dplyr::mutate()
 #' df |>
-#'   dplyr::mutate(date = cb_filename_to_datetime(filename, 'date'))
+#'   dplyr::mutate(date = cb_filename_to_datetime(filename, 'date', timezone = 'America/Los_angeles'))
 #' }
 
-cb_filename_to_datetime <- function(x, type = c('datetime', 'date')) {
+cb_filename_to_datetime <- function(x, type = c('datetime', 'date'), timezone = 'America/Los_angeles') {
 
   # datetime
   if (type == 'datetime') {
 
-    lubridate::ymd_hms(stringr::str_extract(x, '[0-9]{8}_[0-9]{6}'), tz = 'America/Los_angeles')
+    lubridate::ymd_hms(stringr::str_extract(x, '[0-9]{8}_[0-9]{6}'), tz = timezone)
 
   # date
   } else if (type == 'date') {
 
-    lubridate::as_date(lubridate::ymd_hms(stringr::str_extract(x, '[0-9]{8}_[0-9]{6}'), tz = 'America/Los_angeles'))
+    lubridate::as_date(lubridate::ymd_hms(stringr::str_extract(x, '[0-9]{8}_[0-9]{6}'), tz = timezone))
 
   }
 
