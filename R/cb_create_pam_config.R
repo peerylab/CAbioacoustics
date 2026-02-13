be7081f3d1b7c12fed8c1f26923b55cea24b943d
jaymwin <jmwiniarski@gmail.com>
2026-02-13 07:38:43 -0600

    add timezone

--- R/cb_create_pam_config.R
+++ R/cb_create_pam_config.R
@@ -4,6 +4,7 @@
 #' @param study_year
 #' @param study_type
 #' @param study_drive
+#' @param study_timezone
 #'
 #' @return
 #' @export
@@ -13,18 +14,20 @@
 #' cb_config_pam_project(
 #'   study_year = lubridate::year(Sys.Date()),
 #'   study_type = 'Sierra_Monitoring',
-#'   study_path = 'Z:'
+#'   study_path = 'Z:',
+#'   study_timezone = 'America/Los_angeles'
 #' )
 #' }
 
-cb_config_pam_project <- function(study_year, study_type, study_drive) {
+cb_config_pam_project <- function(study_year, study_type, study_drive, study_timezone) {
 
   # create config parameters
   config <-
     list(
       study_year = study_year,
       study_type = study_type,
-      study_drive = study_drive
+      study_drive = study_drive,
+      study_timezone = study_timezone
     )
 
   # create folder to store parameters
