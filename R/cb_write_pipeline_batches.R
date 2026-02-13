d3712f35d49031780d7c46ecea17355d66bd39dc
jaymwin <jmwiniarski@gmail.com>
2026-02-13 08:57:37 -0600

    add option to change output path

--- R/cb_write_pipeline_batches.R
+++ R/cb_write_pipeline_batches.R
@@ -2,13 +2,14 @@
 #' Write effort pipeline csv files
 #'
 #' @param df
+#' @param output_path
 #'
 #' @return csv files of effort (i.e., row for each flac file)
 #' @export
 #'
 #' @examples
 
-cb_write_pipeline_batches <- function(df) {
+cb_write_pipeline_batches <- function(df, output_path) {
 
   region <- unique(df$region)
   study <- unique(df$study_type)
@@ -17,7 +18,7 @@ cb_write_pipeline_batches <- function(df) {
   df |>
     dplyr::select(file_name) |>
     write.table(
-      stringr::str_glue(here::here('effort_pipelines/{region}_{study}_{csv_num}_pipeline.csv')),
+      output_path,
       row.names = FALSE,
       col.names = FALSE
     )
