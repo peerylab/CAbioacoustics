4c4662f9aafb71b5728d8952392c95c7cbc083ba
jaymwin <jmwiniarski@gmail.com>
2026-02-27 08:00:28 -0600

    improved S3 df function

--- R/cb_get_s3_df.R
+++ R/cb_get_s3_df.R
@@ -1,26 +1,45 @@
 
-#' Return a data frame of FLAC and JSON file paths from UW-Madison S3 bucket
+#' Return a data frame of S3 bucket contents
 #'
-#' @return A data frame of S3 FLAC and JSON file paths
+#' @param prefix Search a particular folder or not; default is NULL
+#' @param include_folders Whether to include folder markers or not; default is FALSE
+#'
+#' @return A `tibble` of S3 keys and metadata
 #' @export
 #'
 #' @examples
 #' \dontrun{
-#' # return tibble of S3 FLAC and JSON file paths
-#' s3_flac_df <- cb_get_s3_df()
+#' # return tibble of S3 keys and metadata for one folder
+#' nest_video_df <- cb_get_s3_df(prefix = "Nest_Video_Data/")
+#'
+#' # or the whole S3 bucket
+#' s3_df <- cb_get_s3_df()
 #' }
 
-cb_get_s3_df <- function() {
+cb_get_s3_df <- function(prefix = NULL, include_folders = FALSE) {
 
-  aws.s3::get_bucket_df(
-    bucket = "mpeery-archive",
-    region = "",
-    base_url = "s3.drive.wisc.edu",
-    key = keyring::key_get('aws_access_key_id'),
-    secret = keyring::key_get('aws_secret_access_key'),
-    # it takes a while for this to run (~30 mins?); needs to be Inf to read in all files
-    max = Inf
-  ) |>
-    tibble::as_tibble()
+  s3_df <-
+    aws.s3::get_bucket_df(
+      bucket = "mpeery-archive",
+      prefix = prefix,
+      region = "",
+      base_url = "s3.drive.wisc.edu",
+      key = keyring::key_get("aws_access_key_id"),
+      secret = keyring::key_get("aws_secret_access_key"),
+      max = Inf
+    ) |>
+    tibble::as_tibble() |>
+    janitor::clean_names()
+
+  # remove folder markers unless explicitly requested
+  if (include_folders == FALSE) {
+
+    s3_df <-
+      s3_df |>
+      dplyr::filter(!grepl("/$", key))
+
+  }
+
+  return(s3_df)
 
 }
