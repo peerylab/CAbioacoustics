60db1db67efe5e10085d310dd979b8869cbad9d2
jaymwin <jmwiniarski@gmail.com>
2026-03-02 10:22:15 -0600

    also updated function to connect to S3 database

--- R/cb_connect_s3_db.R
+++ R/cb_connect_s3_db.R
@@ -1,18 +1,20 @@
 
 #' Connect to database containing UW-Madison S3 contents
 #'
-#' @return `duckdb` connection
+#' @param db_path Path to directory containing S3 `DuckDB`
+#'
+#' @return `DuckDB` connection
 #' @export
 #'
 #' @examples
 #' \dontrun{
 #' # connect to duckdb of S3 file paths
-#' cb_connect_s3_db()
+#' cb_connect_s3_db(db_path = "Z:/Bioacoustics/Acoustic_Code/s3_bucket_contents")
 #' }
 
-cb_connect_s3_db <- function() {
+cb_connect_s3_db <- function(db_path) {
 
   # connect to local s3 database
-  conn_s3 <<- DBI::dbConnect(duckdb::duckdb(dbdir = "Z:/s3_bucket_contents/s3_files.duckdb"))
+  conn_s3 <<- DBI::dbConnect(duckdb::duckdb(dbdir = stringr::str_glue("{db_path}/s3_files.duckdb")))
 
 }
