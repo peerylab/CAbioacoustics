7cff5bd89e613b7b58731a87986814878dcccd6c
jaymwin <jmwiniarski@gmail.com>
2026-05-15 13:48:18 -0500

    fixed it up a bit

--- R/cb_robocopy.R
+++ R/cb_robocopy.R
@@ -1,44 +1,74 @@
 
 #' Use `robocopy` to copy audio data from external hard drives
 #'
-#' @param source
-#' @param destination
-#' @param threads
-#' @param retries
-#' @param wait
-#' @param log_file
-#' @param show_output
+#' @param source_drive Hard drive letter
+#' @param destination Directory to copy hard drive files
+#' @param threads Number of threads to use; for HDD may be better to use 8; SSD 16 or 32?
+#' @param retries Number of times to retry copying a file if it fails
+#' @param wait Time in seconds to wait for retry
+#' @param log_dir Directory to store log files
+#' @param exclude_recycle Exclude RECYCLE BIN files
+#' @param verbose Print log output in console, or hide it
 #'
 #' @returns
 #' @export
 #'
 #' @examples
+#' \dontrun{
+#' cb_robocopy(
+#' source_drive = "D:",
+#' destination = "C:/Users/jmwin/Documents/audio_data_backup",
+#' threads = 16,
+#' log_dir = "C:/Users/jmwin/Documents",
+#' exclude_recycle = TRUE,
+#' verbose = TRUE
+#' )
+#' }
 
-cb_robocopy <- function(source, destination, threads = 16, retries = 3, wait = 5, log_file, show_output = TRUE) {
+cb_robocopy <- function(source_drive, destination, threads = 16, retries = 3, wait = 5, log_dir, exclude_recycle = TRUE, verbose = TRUE) {
 
-  if (is.null(log_file)) {
+  # extract drive letter to get volume label (name of hard drive)
+  drive_letter <- sub("^(.:).*", "\\1", source_drive)
+  label <- CAbioacoustics:::get_volume_label(drive_letter)
 
-    log_file <- file.path(destination, "robocopy_log.txt")
+  # create destination folder
+  destination <- file.path(destination, label)
+  dir.create(destination, recursive = TRUE, showWarnings = FALSE)
 
-  }
+  # create log file folder and log file based on volume label
+  dir.create(log_dir, recursive = TRUE, showWarnings = FALSE)
+  log_file <- file.path(log_dir, paste0(label, "_robocopy.log"))
 
-  cmd <-
-    sprintf(
-      'robocopy "%s" "%s" /E /Z /R:%d /W:%d /MT:%d /TEE /LOG:"%s"',
-      source,
-      destination,
-      retries,
-      wait,
-      threads,
-      log_file
+  # set argument commands
+  args <-
+    c(
+      shQuote(source_drive),
+      shQuote(destination),
+      "/E",
+      "/Z",
+      paste0("/R:", retries),
+      paste0("/W:", wait),
+      paste0("/MT:", threads),
+      paste0('/LOG:', shQuote(log_file))
     )
 
-  if (show_output) {
+  # show log output or not
+  if (!verbose) {
 
-    message("Running:\n", cmd)
+    args <- c(args, "/NP", "/NFL", "/NDL")
 
   }
 
-  system(cmd, intern = FALSE)
+  # ignore RECYCLE BIN files
+  if (exclude_recycle) {
+
+    args <- c(args, "/XD", "$RECYCLE.BIN", "System Volume Information")
+
+  }
+
+  # run command
+  message("Running robocopy: ", source_drive, " -> ", destination)
+
+  system2("robocopy", args = args)
 
 }
